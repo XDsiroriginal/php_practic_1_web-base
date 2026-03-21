213fa1b6f53403b6685b0a8cd5cb8f7eac7754cf
gr434_hrdev <anna.sawhenko1984@gmail.com>
2026-03-21 14:03:44 +0700

    praktic 1 final

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gr434_hrdev/php_practic_1_web-base',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ef4b1085d01330035fce76be5bcf2bc1cfb44eec',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gr434_hrdev/php_practic_1_web-base' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ef4b1085d01330035fce76be5bcf2bc1cfb44eec',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
