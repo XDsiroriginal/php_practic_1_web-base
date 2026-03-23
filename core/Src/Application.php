f6b5f5de0d7967c625cc00a04a238e17cd52a740
XDsiroriginal <anna.sawhenko1984@gmail.com>
2026-03-24 01:50:07 +0700

    global session and redirect to "/go"

--- core/Src/Application.php
+++ core/Src/Application.php
@@ -13,22 +13,26 @@ class Application
    private Route $route;
    private Capsule $dbManager;
 
-   public function __construct(Settings $settings)
-   {
-       $this->settings = $settings;
-       $this->route = new Route();
-       $this->dbManager = new Capsule();
-   }
+    public function __construct(Settings $settings)
+    {
+        $this->settings = $settings;
+        $this->route = new Route($this->settings->getRootPath());
+        $this->dbManager = new Capsule();
+    }
+    public function __get($key)
+    {
+        switch ($key) {
+            case 'settings':
+                return $this->settings;
+            case 'route':
+                return $this->route;
+            default:
+                throw new Error('Accessing a non-existent property');
+        }
+    }
 
-   public function __get($key)
-   {
-       if ($key === 'settings') {
-           return $this->settings;
-       }
-       throw new Error('Accessing a non-existent property');
-   }
 
-   private function dbRun()
+    private function dbRun()
    {
        $this->dbManager->addConnection($this->settings->getDbSetting());
        $this->dbManager->setEventDispatcher(new Dispatcher(new Container));
@@ -42,4 +46,6 @@ class Application
        $this->route->setPrefix($this->settings->getRootPath());
        $this->route->start();
    }
+
+
 }
